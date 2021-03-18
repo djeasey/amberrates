55cc867ade5822de722b1400c7ce2f7c969a556c
djeasey <56751556+djeasey@users.noreply.github.com>
2021-03-18 18:21:14 +1100

    Tidy up & switch to local time

--- api.js
+++ api.js
@@ -17,6 +17,7 @@ $(document).ready(function(){
 		var fixedMarket = 0.2486;
 		var fixedPriceProtection = 0.77;
 		var fixedCombined = fixedCarbonNeutral + fixedEnvCert + fixedMarket + fixedPriceProtection;
+
 		var fixedOffPeak = 3.773;
 		var fixedPeak = 16.863;
 		var fixedShoulder = 6.765;
@@ -28,58 +29,50 @@ $(document).ready(function(){
 		var dailyCombined = dailyAmber + dailyMetering + dailyNetwork + dailyNetworkPFIT;
 
 		$(".rates").append("Daily Cost - ");
-		$(".rates").append(dailyCombined.toFixed(2));
-		$(".rates").append("<br>");
+		$(".rates").append(dailyCombined.toFixed(0));
+		$(".rates").append("c/day<br>");
 		$(".rates").append("<br>");
 
-		$.each(response.data.variablePricesAndRenewables, function( index, value ) {
-			var dateAsNumber = Date.parse(value.period + '.000+10:00');
-			var date = new Date(dateAsNumber);
-			var currentdate = new Date();
-			var dayOfWeek = date.getUTCDay();
-			var hour = date.getUTCHours();
-			var minutes = date.getUTCMinutes();
-			var dateString = date.toLocaleTimeString();;
-			var timeOfUse;
-			var fixedCost;
-			if (hour == 12 && minutes == 30) {
-				timeOfUse = 'offpeak';
-			} else if (hour >= 13 && hour <= 20) {
-				timeOfUse = 'offpeak';
-			} else if (hour == 21 && minutes == 0) {
-				timeOfUse = 'offpeak';
-			} else if (dayOfWeek >= 1 && dayOfWeek <= 5) {
-				if (hour == 5 && minutes == 30) {
-					timeOfUse = 'peak';
-				} else if (hour >= 6 && hour <= 10) {
-					timeOfUse = 'peak';
-				} else if (hour == 11 && minutes == 0) {
-					timeOfUse = 'peak';
+		$.each(response.data.variablePricesAndRenewables, function(index, value) {
+			var endTimeWithZone = value.period + '.000+10:00'
+			var endTimeAsNumber = Date.parse(endTimeWithZone);
+			var endTime = new Date(endTimeAsNumber);
+			var currentTime = new Date();
+
+			var HoursDiff = (endTime - currentTime) / 36e5;
+			if (HoursDiff >= 0 && HoursDiff < 6) {
+				var startTime = new Date(endTime - 18e5);
+				var startTimeString = startTime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false});
+				var endTimeString = endTime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false});
+
+				var startDay = startTime.getDay();
+				var startHour = startTime.getHours();
+				var startMinutes = startTime.getMinutes();
+				
+				var timeOfUse;
+				var fixedCost;
+
+				if (startHour >= 22 || startHour <= 6) {
+					timeOfUse = 'OffP';
+					fixedCost = fixedCombined + fixedOffPeak;
+				} else if (startDay >= 1 && startDay <= 5 && startHour >= 15 && startHour <= 20) {
+					timeOfUse = 'Peak';
+					fixedCost = fixedCombined + fixedPeak;
 				} else {
-					timeOfUse = 'shoulder';
+					timeOfUse = 'Shou';
+					fixedCost = fixedCombined + fixedShoulder;
 				}
-			} else {
-				timeOfUse = 'shoulder';
-			}
 
-			if (timeOfUse == 'peak') {
-				fixedCost = fixedCombined + fixedPeak;
-			} else if (timeOfUse == 'shoulder') {
-				fixedCost = fixedCombined + fixedShoulder;
-			} else if (timeOfUse == 'offpeak') {
-				fixedCost = fixedCombined + fixedOffPeak;
-			}
-
-			var hoursDiff = Math.abs(currentdate - date) / 36e5;
-			if (hoursDiff < 4) {
-				$(".rates").append(dateString);
+				$(".rates").append(startTimeString);
+				$(".rates").append("-");
+				$(".rates").append(endTimeString);
 				$(".rates").append(" (");
 				$(".rates").append(timeOfUse);
 				$(".rates").append(") ");
 				$(".rates").append(" - ");
 				var cost = parseFloat(value.wholesaleKWHPrice) * lossFactor + fixedCost;
-				$(".rates").append(cost.toFixed(2));
-				$(".rates").append("<br>");
+				$(".rates").append(cost.toFixed(0));
+				$(".rates").append("c/kWh<br>");
 			}
 
 		});
